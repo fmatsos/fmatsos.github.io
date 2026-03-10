bf7aa68a3ca70aeeec461d5e7d74f033589ddf23
Franck Matsos <github@franck.matsos.fr>
2026-03-10 07:39:25 +0100

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
-  site: 'https://franck.matsos.fr',
+  site: 'https://www.matsos.dev',
   integrations: [icon()],
   build: {
     inlineStylesheets: 'auto',
